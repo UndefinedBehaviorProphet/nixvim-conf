a7ed4dab426137a372eda5d299c8fc79560cbebc
UndefinedBehaviourProphet <206461177+UndefinedBehaviorProphet@users.noreply.github.com>
2025-12-26 03:07:27 +0100

    Add treesitter with all available languages and enable basic lsp

--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ...}:{
   imports = [
     ./bufferline.nix 
     ./snacks.nix
@@ -12,4 +12,25 @@
   # plugins.lazy.enable = true;
   plugins.tmux-navigator.enable = true;
   plugins.blame.enable = true;
+
+  plugins.lsp = {
+    enable = true;
+
+  };
+
+  plugins.treesitter = {
+    enable = true;
+    
+
+
+    highlight.enable = true;
+    indent.enable = true;
+    folding.enable = false;
+    grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
+  };
+  plugins.cmp-treesitter.enable = true;
+
+  plugins.cmp.enable = true;
+
+  plugins.cmp-nvim-lsp.enable = true;
 }
