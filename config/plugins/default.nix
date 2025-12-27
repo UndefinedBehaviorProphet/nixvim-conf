a80bc54c885d8489d9bb0cb8a3f3905de967fbca
UndefinedBehaviourProphet <206461177+UndefinedBehaviorProphet@users.noreply.github.com>
2025-12-27 16:53:28 +0100

    Enable LSP, some language servers and blink for auto completions

--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ...}:{
+{ pkgs, lib, ...}:{
   imports = [
     ./bufferline.nix 
     ./snacks.nix
@@ -13,12 +13,44 @@
   # plugins.lazy.enable = true;
   plugins.tmux-navigator.enable = true;
   # plugins.blame.enable = true;
-
+  
   plugins.lsp = {
     enable = true;
     servers = {
-      nil_ls.enable = true;
-      lua_ls.enable = true;
+      # Nix
+      nixd.enable = true;
+
+      # Web
+      ts_ls.enable = true;
+      html.enable = true;
+      cssls.enable = true;
+      jsonls.enable = true;
+
+      # Backend
+      pyright.enable = true;
+      gopls.enable = true;
+      rust_analyzer = {
+        enable = true;
+        installRustc = true;
+        installCargo = true;
+      };
+      
+      # Config / Infra
+      yamlls.enable = true;
+      bashls.enable = true;
+      dockerls.enable = true;
+    };
+  };
+
+  plugins.blink-cmp  = {
+    enable = true;
+    settings = {
+      keymap = { preset = "default"; };
+      appearance = {
+        use_nvim_cmp_as_default = true;
+        nerd_font_variant = "mono";
+      };
+      signature = { enabled = true; }; 
     };
   };
 
@@ -30,7 +62,12 @@
     grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
   };
 
-  plugins.cmp.enable = true;
+  plugins.cmp = {
+    enable = true;
+    autoEnableSources = true;
+  };
   plugins.cmp-treesitter.enable = true;
   plugins.cmp-nvim-lsp.enable = true;
+  plugins.cmp-buffer.enable = true;
+  plugins.cmp-path.enable = true;
 }
